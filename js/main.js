03d2b773ce5c7a992c232f192a68089fa47cbcf0
Anastasiia Buryak <be.ge.instagram@gmail.com>
2020-02-04 15:24:16 +0200

    add js burger

--- js/main.js
+++ js/main.js
@@ -1,59 +1,28 @@
-if( 'ontouchstart' in window ){ var click = 'touchstart'; }
-	else { var click = 'click'; }
+const burgerMenu = () => {
 
+    const menu = document.querySelector('.burger'),
+        menuNav = document.querySelector('.nav-menu'),
+        body = document.querySelector('body');
 
-	$('div.burger').on(click, function(){
+    body.addEventListener('click', (event) => {
 
-			if( !$(this).hasClass('open') ){ openMenu(); } 
-			else { closeMenu(); }
+        let target = event.target;
 
-	});
-	
+        if (target.closest('.burger')) {
+            menu.classList.add('burger-open');
+            menuNav.style.display = 'block';
+        } else if (target.classList.contains('burger')) {
+            menu.classList.remove('burger-open');
+            menuNav.style.display = 'none';
+        } else if (target.tagName !== 'DIV') {
+            menu.classList.remove('burger-open');
+            menuNav.style.display = 'none';
+        } else {
+            return;
+        }
+        
+    });
 
-	$('div.menu ul li a').on(click, function(e){
-		e.preventDefault();
-		closeMenu();
-	});		
+};
 
-
-	function openMenu(){
-		
-		$('div.circle').addClass('expand');
-					
-		$('div.burger').addClass('open');	
-		$('div.x, div.y, div.z').addClass('collapse');
-		$('.menu li').addClass('animate');
-		
-		setTimeout(function(){ 
-			$('div.y').hide(); 
-			$('div.x').addClass('rotate30'); 
-			$('div.z').addClass('rotate150'); 
-		}, 70);
-		setTimeout(function(){
-			$('div.x').addClass('rotate45'); 
-			$('div.z').addClass('rotate135');  
-		}, 120);
-		
-		
-
-	}
-	
-	function closeMenu(){
-
-		$('div.burger').removeClass('open');	
-		$('div.x').removeClass('rotate45').addClass('rotate30'); 
-		$('div.z').removeClass('rotate135').addClass('rotate150');				
-		$('div.circle').removeClass('expand');
-		$('.menu li').removeClass('animate');
-		
-		setTimeout(function(){ 			
-			$('div.x').removeClass('rotate30'); 
-			$('div.z').removeClass('rotate150'); 			
-		}, 50);
-		setTimeout(function(){
-			$('div.y').show(); 
-			$('div.x, div.y, div.z').removeClass('collapse');
-		}, 70);													
-		
-	}
-   
+burgerMenu();
